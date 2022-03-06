3662c4520edaa89ca3048aca1fe2e1a0ae4db531
TerrenceBeckham <terrencebeckham@icloud.com>
2022-03-06 19:12:23 +0200

    made a question class and set it up using null safety

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+
+  Question({required String question, required bool answer}) {
+    var questionText = question;
+    var questionAnswer = answer;
+  }
+}
